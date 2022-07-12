0cb2e3979c1b57a1c9a5c11103e2771a5e572da8
Tim <orcatim@hotmail.com>
2022-07-12 20:49:36 +0000

    stateful components

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="nice to meet you!!" name="Jean" age="43" />
+      <StatefulGreeting greeting="I'm a stateful class component" name="Barry" />
     </div>
   );
 }
--- react-components/src/components/StatefulGreeting.js
+++ react-components/src/components/StatefulGreeting.js
@@ -1,28 +1,9 @@
 import React from "react";
 
-export default class StatefulGreeting extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
-
-  setCount = () => {
-    this.setState(prevState => {
-      return { count: prevState.count + 1 }
-    })
-  }
-
+class StatefulGreeting extends React.Component {
   render() {
-    return (
-      <div className="greeting">
-        <h1>
-          Hello, {this.props.name}, I'm a stateful component!
-        </h1>
-        <h2>You've clicked {this.state.count} times</h2>
-        <button onClick={this.setCount}>Increment Count</button>
-      </div>
-    );
+    return <h1>Hello {this.props.name}!! {this.props.greeting}</h1>
   }
 }
+
+export default StatefulGreeting;
